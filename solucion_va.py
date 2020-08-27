4d9a37413513c169a85834efff6049425534c12e
Miltioxs <miltioxs98@hotmail.com>
2020-08-26 23:12:37 -0600

    Propuesta de refactorizacion version 2 por Milton

--- solucion_va.py
+++ solucion_va.py
@@ -11,25 +11,23 @@ mediana_pH = df.pH.median()
 mediana_azucarResidual = df.residual_sugar.median()
 mediana_acidoCitrico = df.citric_acid.median()
 
-def nivel(mediana_propiedad, columna_propiedad): 
-    for i, propiedad in enumerate(columna_propiedad):
-        if propiedad >= mediana_propiedad:
-            df.loc[i, propiedad] = 'alto'
+
+
+def nivel_propiedad(mediana_propiedad, columna_propiedad, nombre_columna): 
+    for i, propiedad_vino in enumerate(columna_propiedad):
+        if propiedad_vino >= mediana_propiedad:
+            df.loc[i, nombre_columna] = 'alto'
         else:
-            df.loc[i, propiedad] = 'bajo'
-    resultado = df.groupby(propiedad).quality.mean()
-    return print(resultado)
-
-#mandamos los datos de las propiedades a la funcion nivel        
-alcohol = nivel(mediana_alcohol, df.alcohol)
-ph = nivel(mediana_pH, df.pH)
-azucar_residual =  nivel(mediana_azucarResidual, df.residual_sugar) 
-acido_citrico = nivel(mediana_acidoCitrico,  df.citric_acid) 
-
-
-
-  
+            df.loc[i, nombre_columna] = 'bajo'
+    return print(df.groupby(nombre_columna).quality.mean())
     
+
+#Mandamos los datos de las propiedades a la funcion nivel_propiedad       
+alcohol = nivel_propiedad(mediana_alcohol, df.alcohol, 'alcohol')
+ph = nivel_propiedad(mediana_pH, df.pH, 'pH')
+azucar_residual =  nivel_propiedad(mediana_azucarResidual, df.residual_sugar,
+'residual_sugar') 
+acido_citrico = nivel_propiedad(mediana_acidoCitrico,  df.citric_acid, 'citric_acid')
     
 
 
